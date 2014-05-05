f395c9af78d805ff522ee7cdf2ebfd7a7c2b7bd7
ainame <s.namai.2012@gmail.com>
2014-05-06 04:14:22 +0900

    add fixture xcodeproj which created by liftoff

---
+++ spec/fixtures/FixtureProject/FixtureProject/Classes/Delegates/AINAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AINAppDelegate.h
+//  FixtureProject
+//
+//  Created by Satoshi Namai on 5/6/14
+//  Copyright (c) 2014 ainame. All rights reserved.
+//
+
+@interface AINAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ spec/fixtures/FixtureProject/FixtureProject/Resources/Other-Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FixtureProject
+//
+//  Created by Satoshi Namai on 5/6/14
+//  Copyright (c) 2014 ainame. All rights reserved.
+//
+
+@import UIKit;
+
+#import "AINAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AINAppDelegate class]));
+    }
+}
