541d21f4db5597f0a30232abfc05a8c4d74f95ca
ainame <s.namai.2012@gmail.com>
2014-05-06 04:29:25 +0900

    add spec helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'bundler/setup'
 require 'rspec'
+require 'tmpdir'
+require 'fileutils'
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
@@ -17,6 +19,31 @@ class Fixtures
 
     def file(filename)
       File.open(File.join(dir, filename))
+    end
+  end
+
+  class Xcodeproj
+    class << self
+      def setup
+        @tmp_dir = Dir.mktmpdir
+        FileUtils.cp_r(::Fixtures.file('FixtureProject'), @tmp_dir)
+      end
+
+      def teardown
+        FileUtils.remove_entry_secure @tmp_dir
+      end
+
+      def path
+        File.join(@tmp_dir, 'FixtureProject/FixtureProject.xcodeproj')
+      end
+
+      def classes_dir
+        File.join(@tmp_dir, 'FixtureProject/FixtureProject/Classes')
+      end
+
+      def classes_file_for(filename)
+        File.join(classes_dir, filename)
+      end
     end
   end
 end
