86f0cff3ea27f9796f2285eb57d242309ca041a5
ainame <s.namai.2012@gmail.com>
2014-05-07 06:22:21 +0900

    remove switch-case

--- lib/may/path_resolver.rb
+++ lib/may/path_resolver.rb
@@ -55,45 +55,29 @@ module May
   class PathResolver
     def initialize(context)
       @context = context
-      @project_resolver = ProjectResolver.new(@context.root_dir)
-      @template_resolver = TemplateResolver.new(@context.template_dir)
     end
 
     def each(path, template_name)
       raise unless block_given?
-      yield template(:header, template_name), destination(:header, path)
-      yield template(:implementation, template_name), destination(:implementation, path)
-      yield template(:test, template_name), destination(:test, path)
+      yield template_resolver.header_file(template_name), source_project.header_file(path)
+      yield template_resolver.implementation_file(template_name), source_project.implementation_file(path)
+      yield template_resolver.test_file(template_name), test_project.test_file(path)
     end
 
-    def template(type, template_name)
-      case type
-      when :header
-        @template_resolver.header_file(template_name)
-      when :implementation
-        @template_resolver.implementation_file(template_name)
-      when :test
-        @template_resolver.test_file(template_name)
-      end
+    def template_resolver
+      @template_resolver ||= TemplateResolver.new(@context.template_dir)
     end
 
-    def destination(type, path)
-      case type
-      when :header
-        source_project.header_file(path)
-      when :implementation
-        source_project.implementation_file(path)
-      when :test
-        test_project.test_file(path)
-      end
+    def project_resolver
+      @project_resolver ||= ProjectResolver.new(@context.root_dir)
     end
 
     def source_project
-      @source_project ||= DestinationResolver.new(@project_resolver.source_project)
+      @source_project ||= DestinationResolver.new(project_resolver.source_project)
     end
 
     def test_project
-      @test_project ||= DestinationResolver.new(@project_resolver.test_project)
+      @test_project ||= DestinationResolver.new(project_resolver.test_project)
     end
   end
 end
