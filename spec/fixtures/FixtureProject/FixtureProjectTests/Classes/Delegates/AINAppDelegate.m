8e62222ef33f6c0b4864a8afae1daf260001bdaa
ainame <s.namai.2012@gmail.com>
2014-05-07 05:22:40 +0900

    add FixturePorjectTests

---
+++ spec/fixtures/FixtureProject/FixtureProjectTests/Classes/Delegates/AINAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AINAppDelegate.m
+//  FixtureProject
+//
+//  Created by Satoshi Namai on 5/6/14
+//  Copyright (c) 2014 ainame. All rights reserved.
+//
+
+#import "AINAppDelegate.h"
+
+@implementation AINAppDelegate
+
+@end
