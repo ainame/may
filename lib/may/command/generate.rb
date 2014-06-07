4933f8fbba27aa2d9459d026c168d8aed8e8a061
ainame <s.namai.2012@gmail.com>
2014-06-08 05:52:44 +0900

    extract methods to base class

--- lib/may/command/generate.rb
+++ lib/may/command/generate.rb
@@ -6,6 +6,34 @@ module May
 
       self.abstract_command = true
       self.command = 'generate'
+
+      class << self
+        attr_accessor :template_name, :default_super_class
+      end
+
+      def initialize(argv)
+        super
+        @path    = argv.shift_argument
+        @super_class = argv.shift_argument
+        @options = argv.options
+      end
+
+      def validate!
+        super
+        help! 'Can\'t find a PATH' unless @path
+      end
+
+      def run
+        Service::Generate.run(
+          ApplicationContext.new,
+          {
+            path: @path,
+            options: @options,
+            template_name: class.template_name,
+            super_class_name: (@super_class || class.default_super_class),
+          }
+        )
+      end
     end
   end
 end
--- lib/may/command/generate/nsobject.rb
+++ lib/may/command/generate/nsobject.rb
@@ -4,8 +4,6 @@ module May
   class Command
     class Generate
       class NSObject < Generate
-        USING_TEMPLATE_NAME = 'NSObject'
-
         self.command = 'nsobject'
         self.description = <<EOS
 PATH is a file path which you want to generate file without extension name.
@@ -15,30 +13,8 @@ EOS
           ['SUPER_CLASS', :optional],
         ]
 
-
-        def initialize(argv)
-          super
-          @path    = argv.shift_argument
-          @super_class = argv.shift_argument
-          @options = argv.options
-        end
-
-        def validate!
-          super
-          help! 'Can\'t find a PATH' unless @path
-        end
-
-        def run
-          Service::Generate.run(
-            ApplicationContext.new,
-            {
-              path: @path,
-              options: @options,
-              template_name: USING_TEMPLATE_NAME,
-              super_class_name: (@super_class || 'NSObject'),
-            }
-          )
-        end
+        self.template_name = 'NSObject'
+        self.default_super_class = 'NSObject'
       end
     end
   end
